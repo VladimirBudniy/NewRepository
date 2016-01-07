a08f6225b68522b2f6203df1810e692aaa7b032b
Vladimir Budniy <budniy.va@gmail.com>
2016-01-07 13:06:59 +0200

    add correct branch for Struct

---
+++ C project/C project/Test/FirstStructTest/FirstStructTest.c
@@ -0,0 +1,68 @@
+//
+//  FirstStructTest.c
+//  VBStructTask
+//
+//  Created by Vladimir Budniy on 28.12.15.
+//  Copyright © 2015 Vladimir Budniy. All rights reserved.
+//
+
+#include "FirstStructTest.h"
+#include "VBFirstStruct.h"
+#include "VBMacroHeader.h"
+
+// Test 1.
+void VBPrintSequenceNumber() {
+    printf("%s\n\n", "      Test offsetof");
+    printf("Point  1   %lu\n", offsetof(RandomLocation, bool1));
+    printf("Point  2   %lu\n", offsetof(RandomLocation, ll2));
+    printf("Point  3  %lu\n", offsetof(RandomLocation, bool3));
+    printf("Point  4  %lu\n", offsetof(RandomLocation, bool4));
+    printf("Point  5  %lu\n", offsetof(RandomLocation, short5));
+    printf("Point  6  %lu\n", offsetof(RandomLocation, bool6));
+    printf("Point  7  %lu\n", offsetof(RandomLocation, float7));
+    printf("Point  8  %lu\n", offsetof(RandomLocation, bool8));
+    printf("Point  9  %lu\n", offsetof(RandomLocation, int9));
+    printf("Point 10  %lu\n", offsetof(RandomLocation, double10));
+    printf("Point 11  %lu\n", offsetof(RandomLocation, short11));
+    printf("Point 12  %lu\n", offsetof(RandomLocation, bool12));
+    printf("Point 13  %lu\n", offsetof(RandomLocation, short13));
+    printf("Point 14  %lu\n", offsetof(RandomLocation, string14));
+    printf("\n");
+}
+
+TypeLocation checkSize;
+
+// Test 2. Print size the whole struct and some elements
+void VBPrintSizeStruct() {
+    printf("Size the whole struct's -  %lu %lu %lu\n",
+           sizeof(RandomLocation), sizeof(TypeLocation),
+           sizeof(LocationBool));
+    VBPrintNextString;
+    
+    printf("Size of int int9 - %lu", sizeof(checkSize.int9));
+    VBPrintNextString;
+    
+    printf("Size of cahr *string14 - %lu", sizeof(checkSize.string14));
+    VBPrintNextString;
+    
+    printf("Size of bool1 - %lu", sizeof(checkSize.bool1));
+    VBPrintNextString;
+    PrintComment;
+    VBPrintNextString;
+    
+}
+
+// Test for size
+void VBPrintSizeType() {
+    printf("\n");
+    printf("Size of bool - %lu\n", sizeof(bool));
+    printf("Size of short - %lu\n", sizeof(short));
+    printf("Size of float - %lu\n", sizeof(float));
+    printf("Size of int - %lu\n", sizeof(int));
+    printf("Size o double - %lu\n", sizeof(double));
+    printf("Size of char * - %lu\n", sizeof(char *));
+    printf("Size of long long - %lu\n", sizeof(long long));
+}
+
+
+
