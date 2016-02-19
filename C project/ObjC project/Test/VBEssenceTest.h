38639631f0712bcb288925eb02b018127b358eef
Vladimir Budniy <budniy.va@gmail.com>
2016-02-20 01:05:16 +0200

    added methods for children

---
+++ C project/ObjC project/Test/VBEssenceTest.h
@@ -0,0 +1,13 @@
+//
+//  VBEssenceTest.h
+//  C project
+//
+//  Created by Vladimir Budniy on 19.02.16.
+//  Copyright © 2016 Vladimir Budniy. All rights reserved.
+//
+
+#import "VBEssence.h"
+
+@interface VBEssenceTest : VBEssence
+
+@end
---
+++ C project/ObjC project/Test/VBEssenceTest.m
@@ -0,0 +1,13 @@
+//
+//  VBEssenceTest.m
+//  C project
+//
+//  Created by Vladimir Budniy on 19.02.16.
+//  Copyright © 2016 Vladimir Budniy. All rights reserved.
+//
+
+#import "VBEssenceTest.h"
+
+@implementation VBEssenceTest
+
+@end
