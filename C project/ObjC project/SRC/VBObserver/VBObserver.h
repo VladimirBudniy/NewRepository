2b7d557da65a041308b7a96bcab09a1d71bb9bca
Vladimir Budniy <budniy.va@gmail.com>
2016-03-15 00:30:49 +0200

    add class VBObserver

---
+++ C project/ObjC project/SRC/VBObserver/VBObserver.h
@@ -0,0 +1,13 @@
+//
+//  VBObserver.h
+//  C project
+//
+//  Created by Vladimir Budniy on 15.03.16.
+//  Copyright © 2016 Vladimir Budniy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VBObserver : NSObject
+
+@end
---
+++ C project/ObjC project/SRC/VBObserver/VBObserver.m
@@ -0,0 +1,13 @@
+//
+//  VBObserver.m
+//  C project
+//
+//  Created by Vladimir Budniy on 15.03.16.
+//  Copyright © 2016 Vladimir Budniy. All rights reserved.
+//
+
+#import "VBObserver.h"
+
+@implementation VBObserver
+
+@end
