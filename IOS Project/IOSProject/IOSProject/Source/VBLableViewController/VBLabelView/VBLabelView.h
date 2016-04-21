919cb2294c411a301c41806467e67e2582293988
Vladimir Budniy <budniy.va@gmail.com>
2016-04-21 23:11:43 +0300

    added VBLabelView

---
+++ IOS Project/IOSProject/IOSProject/Source/VBLableViewController/VBLabelView/VBLabelView.h
@@ -0,0 +1,13 @@
+//
+//  VBLabelView.h
+//  IOSProject
+//
+//  Created by Vladimir Budniy on 21.04.16.
+//  Copyright © 2016 Vladimir Budniy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBLabelView : UIView
+
+@end
---
+++ IOS Project/IOSProject/IOSProject/Source/VBLableViewController/VBLabelView/VBLabelView.m
@@ -0,0 +1,14 @@
+//
+//  VBLabelView.m
+//  IOSProject
+//
+//  Created by Vladimir Budniy on 21.04.16.
+//  Copyright © 2016 Vladimir Budniy. All rights reserved.
+//
+
+#import "VBLabelView.h"
+
+@implementation VBLabelView
+
+
+@end
