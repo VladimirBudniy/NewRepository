0ae29f0f133ec860c3a63119262924e18feb4b9d
Vladimir Budniy <budniy.va@gmail.com>
2016-04-26 17:25:53 +0300

    default methods of animation - changing frame. added CGGeometry+VBCategory

--- IOS Project/IOSProject/IOSProject/Source/VBLableViewController/VBLabelView/VBLabelView.m
+++ IOS Project/IOSProject/IOSProject/Source/VBLableViewController/VBLabelView/VBLabelView.m
@@ -13,9 +13,8 @@ static const CGFloat    kVBDefaultAnimationDuration = 0.5;
 @interface VBLabelView ()
 @property (nonatomic, assign) NSUInteger squarePosition;
 
-//- (CGRect)frameForSquarePosition:(NSUInteger)squarePosition;
+- (CGRect)frameForSquarePosition:(NSUInteger)squarePosition;
 
-- (CGAffineTransform)transformForSquarePosition:(NSUInteger)squarePosition;
 - (VBLabelLocation)nextSquarePosition;
 
 - (void)setSquarePosition:(NSUInteger)squarePosition animated:(BOOL)animated;
@@ -50,8 +49,7 @@ static const CGFloat    kVBDefaultAnimationDuration = 0.5;
                           delay:0
                         options:UIViewAnimationOptionCurveEaseIn
                      animations:^{
-                         self.label.transform = [self transformForSquarePosition:squarePosition];
-//                         self.label.frame = [self frameForSquarePosition:squarePosition];
+                         self.label.frame = [self frameForSquarePosition:squarePosition];
                      } completion:^(BOOL finished){
                          if (handler) {
                              handler();
@@ -85,61 +83,34 @@ static const CGFloat    kVBDefaultAnimationDuration = 0.5;
     }
 }
 
-- (CGAffineTransform)transformForSquarePosition:(NSUInteger)squarePosition {
+- (CGRect)frameForSquarePosition:(NSUInteger)squarePosition {
+    
     CGFloat xPoint = 0;
     CGFloat yPoint = 0;
     
     CGSize upperViewSize = self.upperView.frame.size;
     CGSize labelSize = self.label.frame.size;
-
+    
     CGFloat xPointUpperRight = upperViewSize.width - labelSize.width;
     CGFloat yPointLowerRight = upperViewSize.height - labelSize.height;
     
-    
     switch (squarePosition) {
         case kVBLabelUpperRightLocation:
-            return CGAffineTransformMakeTranslation(xPointUpperRight, yPoint);
+            xPoint = xPointUpperRight;
+            break;
             
         case kVBLabelLowerRightLocation:
-            return CGAffineTransformMakeTranslation(xPointUpperRight, yPointLowerRight);
+            xPoint = xPointUpperRight;
+            yPoint = yPointLowerRight;
+            break;
             
         case kVBLabelLowerLeftLocation:
-            return CGAffineTransformMakeTranslation(xPoint, yPointLowerRight);
-            
-        default:
-            return CGAffineTransformMakeTranslation(xPoint, yPoint);
+            yPoint = yPointLowerRight;
+            break;
     }
+    
+    return CGRectMakeWithSize(xPoint, yPoint, labelSize);
 }
-
-
-//- (CGRect)frameForSquarePosition:(NSUInteger)squarePosition {
-//    
-//    CGFloat xPoint = 0;
-//    CGFloat yPoint = 0;
-//    
-//    CGSize upperViewSize = self.upperView.frame.size;
-//    CGSize labelSize = self.label.frame.size;
-//    
-//    CGFloat xPointUpperRight = upperViewSize.width - labelSize.width;
-//    CGFloat yPointLowerRight = upperViewSize.height - labelSize.height;
-//    
-//    switch (squarePosition) {
-//        case kVBLabelUpperRightLocation:
-//            xPoint = xPointUpperRight;
-//            break;
-//            
-//        case kVBLabelLowerRightLocation:
-//            xPoint = xPointUpperRight;
-//            yPoint = yPointLowerRight;
-//            break;
-//            
-//        case kVBLabelLowerLeftLocation:
-//            yPoint = yPointLowerRight;
-//            break;
-//    }
-//    
-//    return CGRectMakeWithSize(xPoint, yPoint, labelSize);
-//}
 
 @end
 
