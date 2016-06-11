38c6d37f5b2f4b20ee9686bf477966d4d1693dd9
Vladimir Budniy <budniy.va@gmail.com>
2016-06-11 16:01:31 +0300

    refactor VBImageModel, refactor VBFriendDetailViewController, UserContext

---
+++ IOS Project/FBNewProject/FBNewProject/VBConstants.h
@@ -0,0 +1,13 @@
+//
+//  VBConstants.h
+//  FBNewProject
+//
+//  Created by Vladimir Budniy on 11.06.16.
+//  Copyright © 2016 Vladimir Budniy. All rights reserved.
+//
+
+#ifndef VBConstants_h
+#define VBConstants_h
+
+
+#endif /* VBConstants_h */
